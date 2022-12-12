fab4b9186b445656c4028b14cfbd0c5d112eb868
Tummalapalli Naveen <120359545+naveent0@users.noreply.github.com>
2022-12-12 13:33:02 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom's New Healthy Diner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, spinach & Rocket smoothie')
-streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🐔🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Toast')
